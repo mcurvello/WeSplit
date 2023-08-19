cdf575d06a62e54c54c5f59842e66175d1ebc055
Marcio Curvello <marciocurvello@gmail.com>
2023-08-19 20:59:14 -0300

    adding a segmentedcontrol for tip percentages

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 0
+    @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20, 25, 0]
@@ -26,6 +26,16 @@ struct ContentView: View {
                             Text("\($0) people")
                         }
                     }
+                }
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
                 }
                 Section {
                     Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
