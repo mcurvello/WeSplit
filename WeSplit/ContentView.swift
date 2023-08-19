06e7d8c58ee98f428261bcc5c3803dcbbbdb2c37
Marcio Curvello <marciocurvello@gmail>
2023-08-19 20:50:33 -0300

    creating pickers in a form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,20 +10,28 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20, 25, 0]
         
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL" ))
-                    .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL" ))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
+                }
             }
-            Section {
-                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "BRL"))
-            }
+            .navigationTitle("WeSplit")
         }
     }
 }
